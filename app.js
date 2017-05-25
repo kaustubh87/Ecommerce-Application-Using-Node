3bc4fb76eb488316abb182b25db240bdc727984d
kvin1087 <kaustubh87@gmail.com>
2017-05-25 16:40:27 -0500

    Added nested queries to db

--- app.js
+++ app.js
@@ -8,24 +8,40 @@ mongodb.MongoClient.connect(uri, function(error, db){
     process.exit(1);
   }
 
-  db.collection('sample').insert({ x: 1 } , function(error, result){
+  var doc = {
+    title: 'Mighty Python',
+    year: 1990,
+    director: 'Steven Spielberg',
+    rating: 'A'
+  };
+
+var query = {
+  year: 1980,
+  rating: 'A'
+};
+
+
+  db.collection('movies').insert(doc , function(error, result){
     if(error)
     {
       console.log(error);
       process.exit(1);
     }
 
-    db.collection('sample').find().toArray(function(error, docs){
+    db.collection('movies').find(query).toArray(function(error, docs){
       if(error)
       {
         console.log(error);
         process.exit(1);
+      }else if(docs.length == 0){
+        console.log('No docs found in the database ');
       }
-
-      console.log('Found docs: ');
-      docs.forEach(function(doc){
-        console.log(JSON.stringify(doc));
-      });
+      else{
+        console.log('Found docs: ');
+        docs.forEach(function(doc){
+          console.log(JSON.stringify(doc));
+        });
+      }
       process.exit(0);
     });
   });
