b19d536df0640f53410671571b8fb34a7bdcf452
kvin1087 <kaustubh87@gmail.com>
2017-05-26 08:11:38 -0500

    Basics to Mocha testing

---
+++ mocha-test.js
@@ -0,0 +1,22 @@
+var assert = require('assert');
+
+describe('my feature', function(){
+  it('works', function() {
+    assert.equal('A', 'A');
+  });
+
+  it('fails', function(){
+    assert.throws(function(){
+      throw 'Error!';
+    });
+  });
+});
+
+
+describe('my other feature', function(){
+  it('async', function(){
+    setTimeout(function(){
+      done();
+    }, 25);
+  });
+});
