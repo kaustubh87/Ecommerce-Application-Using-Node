7bc321ca5c0a00bbec84fe58839b4efa0e9c90a7
kvin1087 <kaustubh87@gmail.com>
2017-05-28 18:56:15 -0400

    Added functions

--- assignment/interface.js
+++ assignment/interface.js
@@ -25,7 +25,7 @@ exports.byDirector = function(db, director, callback) {
     var query = {
       director: director
     };
-    db.collection('movies').find(query).sort({'title':1}).toArray(function(error, documents){
+    db.collection('movies').find(query).toArray(function(error, documents){
 
         if(error){
           console.log(error);
