49b8a10623129774d9e3fb70670ff474010447d7
kvin1087 <kaustubh87@gmail.com>
2017-06-03 13:40:57 -0500

    Added product model

--- models.js
+++ models.js
@@ -4,13 +4,18 @@ module.exports = function(wagner) {
   mongoose.connect('mongodb://localhost:27017/test');
 
   var Category = mongoose.model('Category', require('./category'), 'categories');
+  var Product = mongoose.model('Product', require('./product'), 'products');
 
-  wagner.factory('Category', function(){
-      return Category;
+  var models = {
+    Category: Category,
+    Product: Product
+  };
+
+  _.each(models, function(value, key){
+    wagner.factory(key, function(){
+        return value;
+    });
   });
 
-  return {
-    Category: Category
-  };
 
 };
